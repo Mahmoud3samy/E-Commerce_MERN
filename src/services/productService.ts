1bbe065811c7a8c6bd6d2857ef6357fe0258f34b
Mahmoud <samy.m@aicomai.ai>
2025-05-12 08:36:45 +0300

    Product feature

---
+++ src/services/productService.ts
@@ -0,0 +1,23 @@
+import productModel from "../models/productModel";
+
+export const getAllProduct = async () => {
+  return await productModel.find();
+};
+
+export const seedInitialProducts = async () => {
+  const products = [
+    {
+      title: 'Dell Laptop',
+      image: 'https://m.media-amazon.com/images/I/61GfMDJhhhL._AC_SL1500_.jpg',
+      price: 15000,
+      stoke: 10,
+    },
+  ];
+  
+
+  const existingProducts = await getAllProduct();
+
+  if(existingProducts.length === 0) {
+    await productModel.insertMany(products)
+  }
+}
