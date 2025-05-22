a8cc960e415d2e68fc81b8a6b9e54485d727f9a6
Mahmoud <samy.m@aicomai.ai>
2025-05-22 18:26:56 +0300

    Update endpoint for/cart/items PUT

--- src/routes/cartRoute.ts
+++ src/routes/cartRoute.ts
@@ -1,5 +1,5 @@
 import express from 'express';
-import { addItemToCart, getActiveCartForUser } from '../services/cartService';
+import { addItemToCart, getActiveCartForUser, updateItemInCart } from '../services/cartService';
 import validateJWT from '../middlewares/validateJWT';
 import { ExtendRequest } from '../types/extendedRequest';
 
@@ -20,6 +20,13 @@ router.post('/item', validateJWT, async(req: ExtendRequest, res) => {
   const { productId, quantity } = req.body;
   const response = await addItemToCart({ userId, productId, quantity});
   res.status(response.statusCode).send(response.data);
-})
+});
+
+router.put("/items", validateJWT, async (req: ExtendRequest, res) => {
+  const userId = req?.user?._id;
+  const { productId, quantity } = req.body;
+  const response = await updateItemInCart({ userId, productId, quantity });
+  res.status(response.statusCode).send(response.data);
+});
 
 export default router;
