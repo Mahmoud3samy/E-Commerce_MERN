3ad4a5e6cb446e61ffe53f2fa6a26ca0f1675678
Mahmoud <samy.m@aicomai.ai>
2025-05-22 17:55:40 +0300

    Add item to cart/cart/items POST endpoint

--- src/routes/cartRoute.ts
+++ src/routes/cartRoute.ts
@@ -1,17 +1,25 @@
 import express from 'express';
-import { getActiveCartForUser } from '../services/cartService';
+import { addItemToCart, getActiveCartForUser } from '../services/cartService';
 import validateJWT from '../middlewares/validateJWT';
+import { ExtendRequest } from '../types/extendedRequest';
 
 const router = express.Router();
 
 router.get(
   '/',
   validateJWT,
-  async (req, res) => {
-    const userId = req.user._id;
+  async (req: ExtendRequest,res) => {
+    const userId = req?.user?._id;
     const cart = await getActiveCartForUser({ userId });
     res.status(200).send(cart);
   },
 );
+
+router.post('/item', validateJWT, async(req: ExtendRequest, res) => {
+  const userId = req?.user?._id;
+  const { productId, quantity } = req.body;
+  const response = await addItemToCart({ userId, productId, quantity});
+  res.status(response.statusCode).send(response.data);
+})
 
 export default router;
