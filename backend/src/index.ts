f9421ef0d83a7869c0b283e2c5e84f89e0f245a1
Mahmoud <samy.m@aicomai.ai>
2025-06-03 15:35:17 +0300

    All Products in Home page + Integration with API

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,11 +1,11 @@
 import dotenv from "dotenv";
-
 import express from 'express';
 import mongoose from 'mongoose';
 import userRoute from './routes/userRoute';
 import productRoute from "./routes/productRoute";
 import cartRoute from './routes/cartRoute';
 import { seedInitialProducts } from './services/productService';
+import * as cors from "cors";
 
 dotenv.config();
 
@@ -13,6 +13,7 @@ const app = express();
 const port = 3001;
 
 app.use(express.json())
+app.use(cors());
 
 mongoose
   .connect(process.env.DATABASE_URL|| '')
--- backend/src/services/productService.ts
+++ backend/src/services/productService.ts
@@ -9,9 +9,24 @@ export const seedInitialProducts = async () => {
     const products = [
       {
         title: 'Dell Laptop',
-        image: 'https://m.media-amazon.com/images/I/61GfMDJhhhL._AC_SL1500_.jpg',
+        image:
+          'https://m.media-amazon.com/images/I/61GfMDJhhhL._AC_SL1500_.jpg',
         price: 15000,
         stoke: 10,
+      },
+      {
+        title: 'Asus Laptop',
+        image:
+          'https://eg-en.store.asus.com/media/catalog/product/_/l/_lnhetywof5emix4c.png?width=439&height=439&store=en_EG&image-type=image',
+        price: 25000,
+        stoke: 20,
+      },
+      {
+        title: 'HP Laptop',
+        image:
+          'https://encrypted-tbn0.gstatic.com/shopping?q=tbn:ANd9GcTeMdK5bE-k2dYGv88xl1kf0VQ4MMycFtxQSLkkuqzO28r-2EqbK8aqcmjjxt1lbZ8C49Bpn2d_FrvBzSbAkhFLsiomI_DnaGn_6tu4eVwLjijV6dAlneIGTdUJ_cclzsKhSrYEkbFwxBo&usqp=CAc',
+        price: 40000,
+        stoke: 8,
       },
     ];
     
